312eb75fb4b1c8fc5769032cf9f5c172b7edc0bf
fatma372 <fa1640@fayoum.edu.eg>
2024-08-20 16:18:51 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//to do list:
+let form=document.getElementById("form");
+let list=document.getElementById("list");
+let task=document.getElementById("task");
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //new li
+    let li = document.createElement('li');
+    li.textContent=task.value;
+
+    //new delete btn
+    let deleteBtn=document.createElement("button");
+    deleteBtn.textContent="Delete";
+    deleteBtn.classList.add("delete-btn");
+
+    //check button:
+    let checkBtn=document.createElement("button");
+    checkBtn.textContent="check";
+    checkBtn.classList.add("check-btn");
+  
+
+    if(task.value!=''){
+        list.appendChild(li);
+        li.appendChild(deleteBtn);
+        li.appendChild(checkBtn);
+        task.value='';
+    }
+
+    deleteBtn.addEventListener('click', function() {
+        list.removeChild(li);
+    });
+
+    checkBtn.addEventListener('click', function() {
+        li.classList.toggle("checked");
+    });
+  
+});
